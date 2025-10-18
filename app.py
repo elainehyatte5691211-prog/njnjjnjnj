599c75ebc92aae27df6286e897cf7f5731b24348
elainehyatte5691211-prog <elainehyatte5691211@gmail.com>
2025-10-18 22:48:59 +0300

    Update app.py

--- app.py
+++ app.py
@@ -411,4 +411,4 @@ def http_terminals_stream(term_id: str):
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8889, debug=True, threaded=True)
+    app.run(host='0.0.0.0', port=8889, debug=True, threaded=True)
